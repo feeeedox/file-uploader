ba602056c0ad18f58ed564f506b0688d46df555e
feeeedox <mrminecrafttv19@gmail.com>
2026-03-04 21:56:46 +0100

    update default S3 endpoint to empty string

--- server/utils/s3.ts
+++ server/utils/s3.ts
@@ -1,7 +1,7 @@
 import { S3Client } from '@aws-sdk/client-s3';
 
 export const s3 = new S3Client({
-    endpoint: process.env.S3_ENDPOINT || 'https://storage.fedox.ovh',
+    endpoint: process.env.S3_ENDPOINT || '',
     region: process.env.S3_REGION || 'us-east-1',
     credentials: {
         accessKeyId: process.env.S3_ACCESS_KEY || '',
